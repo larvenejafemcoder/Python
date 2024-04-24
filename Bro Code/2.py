02571db63968170b57e2e9e63ac82634579ab098
itzaRinna <dutchismahdick@gmail.com>
2024-04-24 07:33:36 +0700

    new:2

---
+++ Bro Code/2.py
@@ -0,0 +1,25 @@
+# variable = a container for a value. Behaves as the value that it contains
+
+# name = "Bro"
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name +" "+ last_name
+
+# print(type(name))
+print("Hello {0}".format(full_name))
+
+age = 21
+age += 1
+# print(age)
+# print(type(age))
+print("Your age is: " + str(age))
+
+height = 250.5
+# print(height)
+# print(type(height))
+print("Your height is: " + str(height) + "cm")
+
+human = True
+# print(human)
+# print(type(human))
+print("Are you a human?: " + str(human))
