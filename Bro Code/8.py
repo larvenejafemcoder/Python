6b2b13c7ac4cbbcd9c762854af566ce76598e3e0
itzaRinna <dutchismahdick@gmail.com>
2024-04-24 14:27:51 +0700

    finished 10 python program

---
+++ Bro Code/8.py
@@ -0,0 +1,24 @@
+# slicing = create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+# name = "Bro Code"
+
+# first_name = name[:3]
+# last_name = name[4:]
+# funky_name = name[::2]
+# reversed_name = name[::-1]
+
+
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+
+print(website1[slice])
+print(website2[slice])
