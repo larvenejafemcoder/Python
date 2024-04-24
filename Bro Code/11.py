4e841c0c8bf37d52c097cc1f928f63c149ac4488
itzaRinna <dutchismahdick@gmail.com>
2024-04-24 14:50:00 +0700

    new:11,12,13

---
+++ Bro Code/11.py
@@ -0,0 +1,11 @@
+# while loop = a statement that will execute its block of code,
+#              as long as its condition remains true
+
+# while (1==1):
+#     print("Help! I'm stuck in a loop!")
+
+name = ""
+
+while (name == ""):
+    name = input("Enter your FUCKING name: ")
+print("EARTH TO {0} CLEAR!".format(name))
