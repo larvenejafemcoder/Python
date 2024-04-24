b4d844ee34e1c80be01ffd78eca962928ad83d14
itzaRinna <dutchismahdick@gmail.com>
2024-04-24 07:56:30 +0700

    new:3

---
+++ Bro Code/3.py
@@ -0,0 +1,18 @@
+# multiple assignment = allows us to assign multiple varibles at the same time in one line of code
+
+# name = "Bro"
+# age = 21
+# attractive = True
+
+name, age, attractive = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+SpongeBob = Patrick = Sandy = Squidward = 30
+
+print(SpongeBob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
