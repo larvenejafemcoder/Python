3b3da7df40e88ff9ef8e6caec95b33458fbbfa6e
itzaRinna <dutchismahdick@gmail.com>
2024-04-25 13:58:48 +0700

    new:14,15,16

---
+++ Bro Code/15.py
@@ -0,0 +1,18 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza","hamburger","hotdog","spaghetti","pudding"]
+
+food[0] = "sushi"
+
+food.append("icecream")
+food.remove("hotdog")
+food.pop()
+food.insert(0,"cake")
+food.sort()
+# food.clear()
+
+# print(food)
+# print(food[0])
+
+for x in food:
+    print(x)
---
+++ Bro Code/16.py
@@ -0,0 +1,12 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza","hamburger","hotdog"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food)
+print(food[0])
+print(food[0][0])
+print(food[0][1])
