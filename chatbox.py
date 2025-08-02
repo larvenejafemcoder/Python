11a41229168a7dae8bf51d351608584700621752
Commander KernelGhost <111746841+larvenejafemcoder@users.noreply.github.com>
2025-08-02 09:08:21 +0700

    Update chatbox.py

--- chatbox.py
+++ chatbox.py
@@ -1,7 +1,7 @@
 print("Hello, I am an AI chatbox, ready for service!")
 
 def whatsmyname():
-    print("Hello! My name is PookieGPT")
+    print("Okay hello! My name is PookieGPT")
     print("I was written in Python to make you feel at home ")
     
     health = input("How is your day?\n 1. It's good\n 2. Fine I guess\n 3. Not really good lmao\n")
@@ -14,20 +14,25 @@ def whatsmyname():
         print("Aww... sending virtual hugs")
     else:
         print("I couldn't tell how you're feeling, but I'm here anyway")
+        
+while True
+    continue_now = input("\nWhat would you wanna do?\n 1. Talk\n 2. Ask\n 3. Quit\n")
+    if continue_now == "1":
+        whatsmyname()
+    elif continue_now == "2":
+        print("Sure! What do you wanna talk about?")
+    elif continue_now == "3":
+        print("Okay, bye bye~")
+        quit()
+    else:
+        print("I didn't understand that... try 1, 2, or 3 💬")
 
-def continue_chat():
-    while True:
-        continue_now = input("\nWhat would you wanna do?\n 1. Talk\n 2. Ask\n 3. Quit\n")
 
-        if continue_now == "1":
-            whatsmyname()
-        elif continue_now == "2":
-            print("Sure! What do you wanna talk about?")
-        elif continue_now == "3":
-            print("Okay, bye bye~")
-            quit()
-        else:
-            print("I didn't understand that... try 1, 2, or 3 💬")
-            quit()
+def loopFunction()
+    while input(continue_now) !== 1 | 2 | 2
+        continue_chat()
+        
+
+            
 
 continue_chat()
