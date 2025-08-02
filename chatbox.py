7e925b31d21e4dd720d76e396a308d7aea4d03fb
Commander KernelGhost <111746841+larvenejafemcoder@users.noreply.github.com>
2025-08-02 07:32:59 +0700

    Update chatbox.py

--- chatbox.py
+++ chatbox.py
@@ -1,16 +1,32 @@
 print("Hello, I am an AI chatbox, ready for service!")
 
-def continue_chat():
-    continue_now = input("What would you wanna do? 1.Talk 2.Ask 3.Quit\n")
-
-    if continue_now == "1":
-        print("Hello! My name is PookieGPT 💖")
-    elif continue_now == "2":
-        print("Sure! What do you wanna talk about? 🧠")
-    elif continue_now == "3":
-        print("Okay, bye bye~ ✨")
-        quit()
+def whatsmyname():
+    print("Hello! My name is PookieGPT 💖")
+    print("I was written in Python to make you feel at home 🏡")
+    
+    health = input("How is your day?\n 1. It's good\n 2. Fine I guess\n 3. Not really good lmao\n")
+    
+    if health == "1":
+        print("Yay! I'm happy you're doing well 🌞")
+    elif health == "2":
+        print("Hmm, neutral vibe... wanna talk more? 🧃")
+    elif health == "3":
+        print("Aww... sending virtual hugs (づ｡◕‿‿◕｡)づ 💗")
     else:
-        print("I didn't understand that... try 1, 2, or 3 💬")
+        print("I couldn't tell how you're feeling, but I'm here anyway ✨")
+
+def continue_chat():
+    while True:
+        continue_now = input("\nWhat would you wanna do?\n 1. Talk\n 2. Ask\n 3. Quit\n")
+
+        if continue_now == "1":
+            whatsmyname()
+        elif continue_now == "2":
+            print("Sure! What do you wanna talk about? 🧠")
+        elif continue_now == "3":
+            print("Okay, bye bye~ ✨")
+            break
+        else:
+            print("I didn't understand that... try 1, 2, or 3 💬")
 
 continue_chat()
